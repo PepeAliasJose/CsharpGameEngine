ec7d34b0c3bb6b112dd48a29ea1469a1470a7eb7
José Rodríguez Cáceres <peperc781@gmail.com>
2022-06-09 01:01:16 +0200

    Prueba con una ventana con colores
    Se hace una ventana de prueba y un bucle central que actualiza la ventana cada 16 ms

---
+++ GameEngineS/Demo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameEngineS.motor;
+using System.Drawing;
+
+namespace GameEngineS
+{
+    class Demo : Ventana
+    {
+        //base se usa para acceder a los miembros de la clase base desde una clase derivada
+        public Demo() : base(new vector(800, 600), "Illo es una demo") { 
+        }
+        int frame = 0;
+        int cont = 1;
+        public override void Actualiza()
+        {
+            Console.WriteLine($"Fotogramas: {frame}");
+            frame++;
+           
+        }
+
+        public override void carga() {
+
+            
+
+        }
+
+        public override void Pinta()
+        {
+            if (frame % 60 == 0)
+            {
+                cont++;
+                if (cont == 4)
+                {
+                    cont = 1;
+                }
+            }
+            if (cont == 1)
+            {
+                fondo = Color.Red;
+            }
+            if (cont == 2)
+            {
+                fondo = Color.Green;
+            }
+            if (cont == 3)
+            {
+                fondo = Color.Blue;
+            }
+        }
+    }
+}
---
+++ GameEngineS/motor/vector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngineS.motor
+{
+    public class vector
+    {
+        public float x;
+        public float y;
+
+        public vector() {
+            this.x = cero().x;
+            this.y = cero().y;
+        }
+        public vector(float x,float y)
+        {
+            this.x = x;
+            this.y = y;
+            
+        }
+
+        /// comentarios para cuando se use la funcion en otro lado que sepa que hace
+        /// <summary>
+        /// Devuelve la coordenada como 0,0
+        /// </summary>
+        /// <returns></returns>
+        public vector cero() {
+            return new vector(0,0);
+        }
+    }
+}
